e8f45a5ff09ae6358e95b27d500d50b73de6c3a9
Annie Wong <wong.anni@northeastern.edu>
2026-04-07 20:37:33 -0400

    accounts > users page has rows of data in the table of users, but pop-up need debugging

--- kambaz/users/dao.js
+++ kambaz/users/dao.js
@@ -8,12 +8,26 @@ export default function UsersDao() {
    return newUser; 
  }; 
  const findAllUsers = () => model.find(); 
- const findUserById = (userId) => model.findById(userId); 
- const findUserByUsername = (username) =>  model.findOne({ username: username }); 
+ 
+//  const findUserById = (userId) => model.findById(userId); 
+ const findUserById = async (req, res) => { 
+    const user = await dao.findUserById(req.params.userId); 
+    res.json(user); 
+  };
+
+ const findUserByUsername = (username) =>  model.findOne({ username: username });
  const findUserByCredentials = (username, password) => model.findOne({ username, password });
+ const findUsersByRole = (role) => model.find({ role: role }); // or just model.find({ role }) 
+ const findUsersByPartialName = (partialName) => { 
+  const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive 
+  return model.find({ 
+    $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }], 
+  }); 
+};
+
  const updateUser = (userId, user) => model.updateOne({ _id: userId }, { $set: user }); 
  const deleteUser = (userId) => model.deleteOne({ _id: userId });
  return { 
-   createUser, findAllUsers, findUserById, findUserByUsername, findUserByCredentials, updateUser, deleteUser }; 
+   createUser, findAllUsers, findUserById, findUserByUsername, findUserByCredentials, findUsersByRole, findUsersByPartialName, updateUser, deleteUser }; 
 } 
 
--- kambaz/users/routes.js
+++ kambaz/users/routes.js
@@ -4,7 +4,19 @@ export default function UserRoutes(app) {
  const dao = UsersDao(); 
   const createUser = (req, res) => { }; 
   const deleteUser = (req, res) => { }; 
+  
   const findAllUsers = async (req, res) => { 
+    const { role, name } = req.query; 
+    if (role) { 
+      const users = await dao.findUsersByRole(role); 
+      res.json(users); 
+      return; 
+    } 
+    if (name) { 
+      const users = await dao.findUsersByPartialName(name); 
+      res.json(users); 
+      return; 
+    } 
     const users = await dao.findAllUsers(); 
     res.json(users); 
   };    
